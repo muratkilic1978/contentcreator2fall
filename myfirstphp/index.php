5a2ed890429f18818bad3d45c043d6f45fc11bf4
Murat <muratkilic1978@gmail.com>
2017-08-31 21:26:00 +0200

    I've added new webproject with my first php-code
    I've added myfirstphp webproject with with source-code.

---
+++ myfirstphp/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My first PHP</title>
+</head>
+<body>
+   <ul>
+       <li>Appelsiner</li>
+       <li>Pærer</li>
+       <li>Vandmeloner</li>
+       <li>Kiwi</li>
+       <li>æbler</li>
+   </ul>
+   <form action="" method="post"></form>
+    
+    <?php echo 'Murat Kilic';   ?>
+</body>
+</html>
